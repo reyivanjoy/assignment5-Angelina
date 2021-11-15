5a4de07d889340026efaf6d992a40923bf939634
Justine Louise Rey <Ivan Joy Rey>
2021-11-15 08:12:38 +0800

    Second condition worked

---
+++ Program1.2.py
@@ -0,0 +1,7 @@
+grade = float(input("Grade: "))
+if grade >=96.5 and grade <=100:
+    print("Grade Mark: 1.0")
+    print("Excellent")
+elif grade >=93.5 and grade <=96.4:
+    print("Grade Mark: 1.25")
+    print("Excellent")
